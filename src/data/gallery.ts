342a288931668cbb64fcf0e9bdbbc7d35062936b
Antonio <antonio.carva.96@gmail.com>
2026-05-04 16:58:44 +0200

    Change menu data

---
+++ src/data/gallery.ts
@@ -0,0 +1,32 @@
+export const galleryImages = [
+  {
+    src: "/assets/galeria/local-1.jpg",
+    alt: "Interior del restaurante",
+    className: "lg:row-span-2",
+  },
+  {
+    src: "/assets/menu/tacos-al-pastor.jpg",
+    alt: "Tacos al pastor",
+    className: "",
+  },
+  {
+    src: "/assets/galeria/local-2.jpg",
+    alt: "Ambiente del restaurante",
+    className: "",
+  },
+  {
+    src: "/assets/menu/burrito-tinga-pollo.jpg",
+    alt: "Burrito de tinga de pollo",
+    className: "",
+  },
+  {
+    src: "/assets/menu/nachos-chingones.jpg",
+    alt: "Nachos chingones",
+    className: "lg:col-span-2",
+  },
+  {
+    src: "/assets/menu/tacos-dorados-pollo.jpg",
+    alt: "Tacos dorados de pollo",
+    className: "",
+  },
+];
---
+++ src/data/location.ts
@@ -0,0 +1,4 @@
+export const restaurantAddress = "Calle de Stuart 167, Aranjuez";
+
+export const restaurantMapsUrl =
+  "https://www.google.com/maps/place/Restaurante+Taquer%C3%ADa+Mexicana+B%C3%A9same/@40.0292391,-3.6040386,21z/data=!4m6!3m5!1s0xd42058ae8cdea97:0xa689ddaa30908795!8m2!3d40.0292752!4d-3.6039695!16s%2Fg%2F11j7yj6d8l?entry=tts&g_ep=EgoyMDI2MDQyMi4wIPu8ASoASAFQAw%3D%3D&skid=b120c990-1de8-465a-8381-1be3959c866a";
